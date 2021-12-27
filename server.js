6694e1f36414834551f43f6b1b11cf03dfe4f758
Trammell Hudson <hudson@trmm.net>
2021-12-27 22:52:05 +0100

    server: members and direct messaging work

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ const io = require('socket.io')(http, {
 	},
 });
 
+// if the URL is empty, send the index.html
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/static/index.html");
 });
+
+// serve files out of the ./static directory
 app.use(express.static('./static'));
 
+// topics that are reserved for the server
 const reserved_topics = {
 	"to": 1,
 	"room": 1,
@@ -31,20 +35,22 @@ const reserved_topics = {
 
 function room_join(socket, room)
 {
-	console.log(socket.id + ":" + room + ":joining");
-	const members = io.sockets.adapter.rooms.get(room);
+	console.log(socket.id + ":" + room + ":join");
+	const member_set = io.sockets.adapter.rooms.get(room);
+	const members = member_set ? Array.from(member_set) : [];
+	console.log("members=", members);
 
 	socket.room = room;
 	socket.join(room);
-	socket.to(socket.id).emit('members', room, members);
-	socket.broadcast.to(socket.room).emit('connected', socket.id);
+	socket.broadcast.to(room).emit('connected', socket.id);
+	io.to(socket.id).emit('members', room, members);
 }
 
 function room_leave(socket)
 {
 	if (!socket.room)
 		return;
-	console.log(socket.id + ":" + socket.room + ":leaving");
+	console.log(socket.id + ":" + socket.room + ":leave");
 	socket.broadcast.to(socket.room).emit('disconnected', socket.id);
 	socket.leave(socket.room);
 	socket.room = null;
@@ -52,9 +58,11 @@ function room_leave(socket)
 
 
 io.on('connection', (socket) => {
-	console.log(socket.id, 'connect', socket.handshake.address);
+	console.log(socket.id + ":*:connect", socket.handshake.address);
 
-	room_join(socket, "default");
+	socket.room = null;
+
+	//room_join(socket, "default");
 
 	socket.on('disconnect', () => {
 		// they are gone, so tell the room
@@ -68,12 +76,18 @@ io.on('connection', (socket) => {
 		room_join(socket, room);
 	});
 
+	// private message to another socket id or room
 	socket.on('to', (dest,topic,msg) => {
-		// private message to another socket id or room
-		console.log(socket.id + ":dest=" + dest + ":topic=" + topic, "msg=", msg);
-		socket.to(dest).emit(topic, socket.id, msg);
+		// don't allow them to spoof reserved topics
+		if (topic in reserved_topics)
+			return;
+
+		console.log(socket.id + ":" + dest + ":" + topic, msg);
+		io.to(dest).emit(topic, socket.id, msg);
 	});
 
+	// otherwise echo any normal message to everyone in the room
+	// prefixing it with the socket id of the sender
 	socket.onAny((topic,msg) => {
 		// don't forward any reserved topics
 		if (topic in reserved_topics)
